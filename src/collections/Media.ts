a5965a656470efd6133cea13611a62fe8b7600fa
Levente Bognar <lajos93@users.noreply.github.com>
2025-06-03 12:40:39 +0200

    Update Media.ts

--- src/collections/Media.ts
+++ src/collections/Media.ts
@@ -8,9 +8,6 @@ export const Media: CollectionConfig = {
   upload: {
     disableLocalStorage: true,
   },
-  upload: {
-    disableLocalStorage: true, // Disable local storage to use S3
-  },
   fields: [
     {
       name: 'alt',
