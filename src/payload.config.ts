b1d836970e3daf241eb88a215c84abff58b1eded
Levente Bognar <lajos93@users.noreply.github.com>
2025-06-03 10:11:57 +0200

    add AWS storage plugin
    fix files not being uploaded to AWS remote sever

--- src/payload.config.ts
+++ src/payload.config.ts
@@ -2,9 +2,10 @@
 import { postgresAdapter } from '@payloadcms/db-postgres'
 import { payloadCloudPlugin } from '@payloadcms/payload-cloud'
 import { lexicalEditor } from '@payloadcms/richtext-lexical'
-import path from 'path'
+import { s3Storage } from '@payloadcms/storage-s3'
 import { buildConfig } from 'payload'
 import { fileURLToPath } from 'url'
+import path from 'path'
 import sharp from 'sharp'
 
 import { Users } from './collections/Users'
@@ -14,6 +15,20 @@ import { Trees } from './collections/Trees'
 
 const filename = fileURLToPath(import.meta.url)
 const dirname = path.dirname(filename)
+
+// Environment variables for S3 storage
+
+const bucket = process.env.S3_BUCKET
+if (!bucket) throw new Error('S3_BUCKET env var is required')
+
+const region = process.env.S3_REGION
+if (!region) throw new Error('S3_REGION env var is required')
+
+const accessKeyId = process.env.S3_ACCESS_KEY_ID
+if (!accessKeyId) throw new Error('S3_ACCESS_KEY_ID env var is required')
+
+const secretAccessKey = process.env.S3_SECRET_ACCESS_KEY
+if (!secretAccessKey) throw new Error('S3_SECRET_ACCESS_KEY env var is required')
 
 export default buildConfig({
   admin: {
@@ -35,7 +50,21 @@ export default buildConfig({
   }),
   sharp,
   plugins: [
-    payloadCloudPlugin(),
+    s3Storage({
+      collections: {
+        media: {
+          prefix: 'images',
+        },
+      },
+      bucket,
+      config: {
+        region,
+        credentials: {
+          accessKeyId,
+          secretAccessKey,
+        },
+      },
+    }),
     // storage-adapter-placeholder
   ],
 })
