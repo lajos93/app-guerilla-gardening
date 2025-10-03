9e489ea755b0c6e0cd38e669d3177c10094f07df
Levente Bognar <lajos93@users.noreply.github.com>
2025-10-03 21:37:11 +0200

    type fixes

--- src/app/(frontend)/[slug]/page.tsx
+++ src/app/(frontend)/[slug]/page.tsx
@@ -6,27 +6,23 @@ export default async function Page({
   params,
   searchParams,
 }: {
-  params: { slug: string }
-  searchParams: { lang?: string }
+  params: Promise<{ slug: string }>
+  searchParams?: Promise<{ lang?: string }>
 }) {
+  const { slug } = await params
+  const { lang } = (await searchParams) || {}
+  const locale: 'hu' | 'en' = (lang as 'hu' | 'en') || 'hu'
+
   const payload = await getPayload({ config })
-
-  const lang = (searchParams.lang as 'hu' | 'en') || 'hu'
-
   const result = await payload.find({
     collection: 'pages',
-    where: {
-      slug: { equals: params.slug },
-    },
+    where: { slug: { equals: slug } },
     limit: 1,
-    locale: lang,
+    locale,
   })
 
   const page = result.docs[0]
-
-  if (!page || page.isRoot) {
-    return <h1>Page not found</h1>
-  }
+  if (!page || page.isRoot) return <h1>Page not found</h1>
 
   return <RenderBlocks blocks={page.layout as Block[]} />
 }
--- src/app/(frontend)/page.tsx
+++ src/app/(frontend)/page.tsx
@@ -1,26 +1,25 @@
-// app/page.tsx
 import { getPayload } from 'payload'
 import config from '@/payload.config'
 import { Block, RenderBlocks } from '../../blocks/RenderBlocks'
 
-export default async function HomePage({ searchParams }: { searchParams: { lang?: string } }) {
+export default async function HomePage({
+  searchParams,
+}: {
+  searchParams?: Promise<{ lang?: string }>
+}) {
+  const { lang } = (await searchParams) || {}
+  const locale: 'hu' | 'en' = (lang as 'hu' | 'en') || 'hu'
+
   const payload = await getPayload({ config })
-
-  // query stringből olvassuk a nyelvet (?lang=hu vagy ?lang=en)
-  const lang = (searchParams.lang as 'hu' | 'en') || 'hu'
-
   const result = await payload.find({
     collection: 'pages',
     where: { isRoot: { equals: true } },
     limit: 1,
-    locale: lang,
+    locale,
   })
 
   const page = result.docs[0]
-
-  if (!page) {
-    return <h1>No homepage set</h1>
-  }
+  if (!page) return <h1>No homepage set</h1>
 
   return <RenderBlocks blocks={page.layout as Block[]} />
 }
